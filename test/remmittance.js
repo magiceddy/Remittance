d1beebc19f9910c85c9bd91c33e019f2630faa2f
magiceddy <rinaldo.rossi.web@gmail.com>
2018-04-08 10:52:50 +0200

    add contructor test

--- test/remmittance.js
+++ test/remmittance.js
@@ -6,5 +6,38 @@ Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 let instance;
 
 contract('Remittance', async accounts => {
-    instance = await Remittance.deployed();
+    const owner = accounts[0];
+    const sender = accounts[1];
+    const exchange = accounts[2];
+
+    beforeEach(async() => {
+        instance = await Remittance.deployed();
+    });
+
+    describe('Contructor', async() => {
+
+        describe('fail case', async() => {
+
+            it('should fail with value in transaction', async() => {
+                try {
+                    const ValueInstance = await Remittance.new({ value: 10 });
+                    assert.isUndefined(ValueInstance, 'Contructor accept transaction with value');
+                } catch (err) {
+                    assert.equal(
+                        err.message, 
+                        'Cannot send value to non-payable constructor', 
+                        'No Revert for value in contructor transaction'
+                    );
+                }
+            });
+        });
+
+        describe('success case', async() => {
+
+            it('should set the correct contract\'s owner', async() => {
+                const contractOwner = await instance.owner();
+                assert.strictEqual(contractOwner, owner, 'Contract creator is not the owner');
+            });
+        });
+    });
 });
